936aed4681b62ebfb759e0cf12ec1a5eeb8279e0
Ross Brandon <ross.brandon@pm.me>
2025-11-01 00:00:39 -0500

    Small improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './plugins/remark-reading-time.mjs';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rossbrandon.dev',
   integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
+  },
+  server: {
+    headers: {
+      'Content-Security-Policy':
+        "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self'; base-uri 'self'; form-action 'self';",
+    },
   },
 });
